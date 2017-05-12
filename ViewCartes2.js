ec84e63bd7353a0350690afa8ddcf4957b81d75a
rmbengui <rmbengui@sefas.com>
2017-05-12 17:00:33 +0200

    mise à jour des cartes personnages
    +
    affichage des cartes

--- ViewCartes2.js
+++ ViewCartes2.js
@@ -148,17 +148,6 @@ imageDesc: {
 
   viewCarte: {
     backgroundColor: '#F5FCFF',
-        borderWidth: 1,
-        borderRadius: 2,
-        borderColor: '#ddd',
-        borderBottomWidth:0,
-        shadowColor: '#000',
-        shadowOffset:{width:0, height:2},
-        shadowOpacity: 0.1,
-        shadowRadius: 2,
-        marginLeft:5,
-        marginRight:5,
-        marginTop:10,
   },
 
 });
